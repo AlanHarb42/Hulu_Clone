7e41263710452b91af3ed420b55b9dda0cdf5a67
Alan Harb <alanharb420@gmail.com>
2021-12-03 14:12:58 +0000

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import HeaderItem from './HeaderItem';
+import {HomeIcon,
+        UserIcon,
+        SearchIcon,
+        BadgeCheckIcon,
+        LightningBoltIcon,
+        CollectionIcon,
+        UsersIcon,
+        } from '@heroicons/react/outline'
+
+function Header() {
+    return(
+        <header className='flex flex-col sm:flex-row m-5
+        justify-between items-center h-auto'>
+
+            <div className='flex flex-grow justify-evenly max-w-2xl'>
+                <HeaderItem title='Home' Icon={HomeIcon}/>
+                <HeaderItem title='Trending' Icon={LightningBoltIcon}/>
+                <HeaderItem title='Verified' Icon={BadgeCheckIcon}/>
+                <HeaderItem title='Collections' Icon={CollectionIcon}/>
+                <HeaderItem title='Search' Icon={SearchIcon}/>
+                <HeaderItem title='Account' Icon={UsersIcon}/>
+            </div>
+            <Image
+                src='https://links.papareact.com/ua6'
+                className='object-contain'
+                width={200}
+                height={100}
+            />
+
+        </header>
+    )
+}
+
+export default Header
---
+++ utils/requests.js
@@ -0,0 +1,25 @@
+const API_KEY = process.env.API_KEY;
+
+export default {
+    fetchTrending: {
+        title: 'Trending',
+        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    },
+    fetchTopRated: {
+        title: 'Top Rated',
+        url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    },
+    fetchActionMovies: {
+        title: 'Action',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    },
+    fetchMystery: {
+        title: 'Mystrey',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    },
+    fetchSciFi: {
+        title: 'Sci-Fi',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    },
+
+}
